3cc816e303e86ccdd31100e5f515b373dbc8ceab
chevalmuscle <me@marcantoinejean.com>
2020-04-29 19:07:36 -0400

    disables word adding if the input is empty in game settings

--- public/index.js
+++ public/index.js
@@ -23,10 +23,12 @@ window.onload = function() {
 
   $("#add-word-button").click(function(e) {
     const newWord = $("#add-word-input").val();
-    self.words.push(newWord);
-    $("#add-word-input").val("");
+    if (newWord !== "") {
+      self.words.push(newWord);
+      $("#add-word-input").val("");
 
-    updateWordListElement();
+      updateWordListElement();
+    }
     return false;
   });
 
